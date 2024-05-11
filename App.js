08af54e7388e9cf7e9c48211a80b9b2c63ad94bc
aditisharma0040 <157268619+aditisharma0040@users.noreply.github.com>
2024-05-11 11:00:27 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import axios from 'axios'; 
+import "./App.css";
+import Container from './Container';
+import Movielist from './Movielist'
+function App() {
+  return (
+    <div className='cont'>
+          <Container/>
+    </div>
+
+   
+  );
+}
+
+export default App;
